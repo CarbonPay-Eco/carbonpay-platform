4c7ee1438c5b478ec7af4687745e5f76432c930e
matheusmacedosantos <matheus.santos@sou.inteli.edu.br>
2025-11-25 11:25:15 -0300

    feat: integrate Purchase entity into data source and simplify API base URL configuration

--- app/backend/src/database/data-source.ts
+++ app/backend/src/database/data-source.ts
@@ -7,6 +7,7 @@ import { TokenizedProject } from "./entities/TokenizedProject";
 import { Retirement } from "./entities/Retirement";
 import { AuditLog } from "./entities/AuditLog";
 import { User } from "../entities/User";
+import { Purchase } from "./entities/Purchase";
 import "dotenv/config";
 import {
   DB_HOST,
@@ -36,6 +37,7 @@ export const AppDataSource = new DataSource({
     TokenizedProject,
     Retirement,
     AuditLog,
+    Purchase,
   ],
   migrations: [],
   subscribers: [],
